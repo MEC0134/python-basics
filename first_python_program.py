afce3b45f45395b5a327e965c48bf972f13ef6b9
M. Enes Celik <m96celik@gmail.com>
2024-11-28 13:51:45 +0300

    first commit

---
+++ first_python_program.py
@@ -0,0 +1,31 @@
+# This is a python comment
+
+# Below is how to declare variables in python, notice the absence of semicolons and explicit type declaring
+# x = 5
+# y = "Hello World!"
+
+# How to concatenate str with a non-string type in python using built in str() method, aka casting
+# print(y + " I am " + str(x) + " years old.")
+
+"""
+Normally we do not explicitly specify data type in python,
+we can however apply casting to a variable using built in methods as
+demonstrated below
+"""
+
+x = int(3)
+y = "John"
+z = float(3)
+
+# print(x, y, z)
+
+
+# Getting the data type of variables
+print(type(y))
+print(type(x))
+print(type(z))
+
+
+
+
+
